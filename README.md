b468ab33325cffaa3ed227762a75d336880e74ca
insight88 <64120339+insight88@users.noreply.github.com>
2020-07-06 21:17:33 +0900

    20200706

--- README.md
+++ README.md
@@ -1,6 +1,8 @@
-# Wetube
+# WeTube
 
-Cloning Youtube with Vanilla and NodeJS     
+Cloning Youtube with Vanilla and NodeJS
+
+## Pages:
 
 - [ ] Home
 - [x] Join
@@ -9,17 +11,6 @@ Cloning Youtube with Vanilla and NodeJS
 - [ ] User Detail
 - [x] Edit Profile
 - [x] Change Password
-- [X] Upload
+- [x] Upload
 - [ ] Video Detail
-- [x] Edit Video    
-
-// package.json 주석
-
-// "start": "nodemon --exec babel-node index.js"
-// --delay 2 : index.js를 부르고 2초 딜레이 후 서버 재시작
-
-// nodemon : 코드를 저장하면 자동으로 서버를 재시작하는 module
-// dependencies : 프로젝트가 실행되기 위해 필수적으로 실행해야 할 modules
-// devDependencies : 프로그래머가 사용하려고 설정한 module
-// npm install nodemon -D : dependencies가 아닌 곳에 nodemon을 설치
-// npm install eslint -g : eslint를 프로젝트에 상관없이 global하게 설치하고 사용
+- [x] Edit Video
--- db.js
+++ db.js
@@ -1,22 +1,20 @@
 import mongoose from "mongoose";
-// mongoose : javascript에서 mongoDB이해할 수 있게 만드는 module
 import dotenv from "dotenv";
+
 dotenv.config();
 
 mongoose.connect(
-    process.env.MONGO_URL,
-    {
-        useNewUrlParser: true,
-        useFindAndModify: false
-    }
-)
-// String으로 된 Database를 요청, 어디에 Database가 저장되어있는지 알려줌
-// mongodb://localhost:포트번호/Database이름
+  process.env.MONGO_URL,
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false
+  }
+);
 
 const db = mongoose.connection;
 
-const handleOpen = () => console.log("✅ Connected to DB");
-const handleError = error => console.log(`❌ Error on DB Connection : ${error}`);
+const handleOpen = () => console.log("✅  Connected to DB");
+const handleError = error => console.log(`❌ Error on DB Connection:${error}`);
 
 db.once("open", handleOpen);
 db.on("error", handleError);
